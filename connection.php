20c7129685fa4c206f61fe0012e0446320f4fa56
Daniel <daniel.trejo.espino@gmail.com>
2023-04-15 14:57:40 -0600

    t1

--- connection.php
+++ connection.php
@@ -1,5 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
+
 
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
