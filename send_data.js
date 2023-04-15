1a83efb95db8ab91493cd9710695f310690cc02a
Daniel <daniel.trejo.espino@gmail.com>
2023-04-15 15:07:37 -0600

    a1

--- send_data.js
+++ send_data.js
@@ -1,17 +1,37 @@
   //   fetch('//www.tarjetavolaris-invex.com.mx/scripts/contacto.php', options)
 //   fetch('http://www.tarjetavolaris-invex.com.mx/scripts/contacto.php', {
-  fetch('https://pruebasdte.space/GitHub/connection.php', {
+//   fetch('https://pruebasdte.space/GitHub/connection.php', {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       nombre: 'pruebaas',
+//       apellidos: 'prueba',
+//       telefono: 'prueba',
+//       mail: 'prueba@prueba.com',
+//       horario: '1'
+//     })
+// }) 
+//   .then(response => {
+//       console.log(`Envio exitoso---- ${response}`);
+//   })
+//   .catch(error => {
+//       console.log('EROR----'+error);
+//   });
+
+
+const datos = new FormData();
+		datos.append('nombre', 'Juan');
+		datos.append('apellidos', 'Pérez');
+		datos.append('telefono', 30);
+
+fetch('https://pruebasdte.space/GitHub/connection.php', {
     method: 'POST',
     headers: {
         'Content-Type': 'application/json'
     },
-    body: JSON.stringify({
-      nombre: 'pruebaas',
-      apellidos: 'prueba',
-      telefono: 'prueba',
-      mail: 'prueba@prueba.com',
-      horario: '1'
-    })
+    body: datos
 }) 
   .then(response => {
       console.log(`Envio exitoso---- ${response}`);
@@ -19,3 +39,16 @@
   .catch(error => {
       console.log('EROR----'+error);
   });
+
+  
+
+
+
+
+
+
+
+
+
+
+
