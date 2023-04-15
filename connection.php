83cc132714a9b79f90f11f8af6ea9d4fb7c5ff49
Daniel <daniel.trejo.espino@gmail.com>
2023-04-15 14:48:10 -0600

    test6

--- connection.php
+++ connection.php
@@ -20,18 +20,19 @@ if (!$conn) {
 }
 echo "Conexión exitosa";
 
-echo $nombre= $_POST["nombre"];
-echo $apellidos = $_POST["apellidos"];
-echo $mail = $_POST["mail"];
-echo $telefono= $_POST["telefono"];
-echo $horario= $_POST["horario"];
+// echo $nombre= $_POST["nombre"];
+// echo $apellidos = $_POST["apellidos"];
+// echo $mail = $_POST["mail"];
+// echo $telefono= $_POST["telefono"];
+// echo $horario= $_POST["horario"];
 echo $fuente= "SITIO INVEX";
-// echo $nombre= 'prueba';
-// echo $apellidos = 'prueba';
-// echo $mail = 'prueba';
-// echo $telefono= 'prueba';
-// echo $horario= 'prueba';
-// echo $fuente= "SITIO INVEX";
+
+echo $nombre= 'prueba';
+echo $apellidos = 'prueba';
+echo $mail = 'prueba';
+echo $telefono= 'prueba';
+echo $horario= 'prueba';
+
 
 
 $sql_insert="INSERT INTO contacto (cont_nombre, cont_apellidos,cont_mail,cont_telefono,cont_horario,cont_fuente)
