0309589cf87adae90dbfb90e5f2a985d52d28ebc
Daniel <daniel.trejo.espino@gmail.com>
2023-04-15 14:28:42 -0600

    select mysql

--- connection.php
+++ connection.php
@@ -6,10 +6,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
  
-$servername = "sql141.main-hosting.eu"; // Cambia esto por el nombre de tu servidor MySQL
-$dbname = "u466684088_prueba_1"; // Cambia esto por el nombre de tu base de datos
-$username = "u466684088_prueba_1"; // Cambia esto por tu nombre de usuario MySQL
-$password = "Prueba_1prueba_1"; // Cambia esto por tu contraseña de usuario MySQL
+$servername = "sql141.main-hosting.eu"; 
+$dbname = "u466684088_prueba_1"; 
+$username = "u466684088_prueba_1"; 
+$password = "Prueba_1prueba_1"; 
 
 // Crear una conexión
 $conn = mysqli_connect($servername, $username, $password, $dbname);
@@ -34,12 +34,30 @@ echo $horario= 'prueba';
 echo $fuente= "SITIO INVEX";
 
 
-$sql="INSERT INTO contacto (cont_nombre, cont_apellidos,cont_mail,cont_telefono,cont_horario,cont_fuente)
+$sql_insert="INSERT INTO contacto (cont_nombre, cont_apellidos,cont_mail,cont_telefono,cont_horario,cont_fuente)
       VALUES ('$nombre','$apellidos','$mail','$telefono','$horario','$fuente') ";
-if (mysqli_query($conn, $sql)) {
+if (mysqli_query($conn, $sql_insert)) {
    echo json_encode("Nuevo registro insertado correctamente");
 } else {
    echo  json_encode( "Error: " . $sql . "<br>" . mysqli_error($conn));
+}
+echo '<hr>';
+$sql_consulta='SELECT * FROM contacto';
+
+$result = mysqli_query($conn,$sql_consulta);
+if (mysqli_num_rows($result) > 0) {
+   // Crear un array vacío para almacenar los resultados
+   $data = array();
+   
+   // Recorrer los resultados y agregar cada fila al array
+   while($fila = mysqli_fetch_assoc($result)) {
+       $data[] = $fila;
+   }
+   
+   // Convertir el array a un objeto JSON y mostrarlo en pantalla
+   echo json_encode($data);
+} else {
+   echo "No se encontraron resultados";
 }
 
 
