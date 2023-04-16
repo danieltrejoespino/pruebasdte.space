155b1af6e646a30fb2c162771aeafd1fae9c6242
Daniel <daniel.trejo.espino@gmail.com>
2023-04-16 00:04:40 -0600

    f4

--- send_data.js
+++ send_data.js
@@ -21,12 +21,12 @@
 //   });
 
 
-const datos = new FormData();
-		datos.append('nombre', 's');
-		datos.append('apellidos', 'Pérez');
-		datos.append('telefono', 1234567890);
-		datos.append('mail', 'prueba@prueba.com');
-		datos.append('horario', 1);
+// const datos = new FormData();
+		// datos.append('nombre', 's');
+		// datos.append('apellidos', 'Pérez');
+		// datos.append('telefono', 1234567890);
+		// datos.append('mail', 'prueba@prueba.com');
+		// datos.append('horario', 1);
 
 // fetch('https://pruebasdte.space/GitHub/contacto.php', {
 //     method: 'POST',
@@ -43,12 +43,10 @@ const datos = new FormData();
 
    
   fetch('https://pruebasdte.space/GitHub/consulta.php')
-  .then(response => response.json()) // Si se espera una respuesta en formato JSON
-  .then(json => {
-    console.log(json)
-  })
-  .catch(error => {
-    console.log(error)
+  .then(response => response.json())
+  .then(data => {
+    // Aquí puedes trabajar con los datos del JSON
+    console.log(data);
   });
 
 
---
+++ traerdatos.php
@@ -0,0 +1,18 @@
+<?php
+
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+ 
+
+$ch = curl_init();
+   curl_setopt($ch, CURLOPT_URL, 'https://pruebasdte.space/GitHub/consulta.php'); 
+   curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+   curl_setopt($ch, CURLOPT_HEADER, 0); 
+   $data = curl_exec($ch); 
+   var_dump($data);
+   curl_close($ch); 
+
+
+?>
